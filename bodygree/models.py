50e765ca39c5ef546260f0aef181c56e783b059f
20221796 <dorisjbssu@gmail.com>
2022-11-22 18:57:13 +0900

    파일 크기 때문에 커밋 수정

--- bodygree/models.py
+++ bodygree/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class login(models.Model):
+    id = models.CharField(max_length=100, primary_key=True, unique=True)
+    pw = models.CharField(max_length=200)
--- bodygree/urls.py
+++ bodygree/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('test', views.test, name='test'),
+    path('home', views.home, name='home'), # 웹사이트 링크 home.html
+    # path("detectme", views.detectme, name='detectme'), # 웹캠 링크
+
+    path("detectme_OX", views.detectme_OX, name="detectme_OX"),
+    path("detectme_XHandsUp", views.detectme_XHandsUp, name="detectme_XHandsUp"),
+    path("detectme_Stretching", views.detectme_Stretching, name="detectme_Stretching"),
+    
+    path('OX', views.OX, name="OX"),
+    path('XHandsUp', views.XHandsUp, name="XHandsUp"),
+    path('Stretching', views.Stretching, name="Stretching"),
 ]
