1015bc13db6b544500eaab5d61f605d9beb56ace
20221796 <dorisjbssu@gmail.com>
2022-11-25 01:44:26 +0900

    척추 측만증 진단구현

--- bodygree/urls.py
+++ bodygree/urls.py
@@ -5,13 +5,19 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('test', views.test, name='test'),
     path('home', views.home, name='home'), # 웹사이트 링크 home.html
+    path('login', views.login, name='login'),
+    path('dignore', views.dignore, name='dignore'),
+    path('result', views.result, name = 'result'),
     # path("detectme", views.detectme, name='detectme'), # 웹캠 링크
 
-    path("detectme_OX", views.detectme_OX, name="detectme_OX"),
-    path("detectme_XHandsUp", views.detectme_XHandsUp, name="detectme_XHandsUp"),
-    path("detectme_Stretching", views.detectme_Stretching, name="detectme_Stretching"),
+    # path("detectme_OX", views.detectme_OX, name="detectme_OX"),
+    # path("detectme_XHandsUp", views.detectme_XHandsUp, name="detectme_XHandsUp"),
+    # path("detectme_Stretching", views.detectme_Stretching, name="detectme_Stretching"),
     
-    path('OX', views.OX, name="OX"),
-    path('XHandsUp', views.XHandsUp, name="XHandsUp"),
-    path('Stretching', views.Stretching, name="Stretching"),
+    # path('OX', views.OX, name="OX"),
+    # path('XHandsUp', views.XHandsUp, name="XHandsUp"),
+    # path('Stretching', views.Stretching, name="Stretching"),
+
+    path('detectme_bodycheck', views.detectme_bodycheck, name='detectme_bodycheck'),
+    path('Bodycheck', views.Bodycheck, name='Bodycheck'), 
 ]
--- bodygree/views.py
+++ bodygree/views.py
@@ -1,10 +1,3 @@
-# from django.shortcuts import render
-
-# from django.views.decorators import gzip
-# from django.http import StreamingHttpResponse
-# import cv2
-# import threading
-
 from glob import glob
 from http.client import HTTPResponse
 from django.shortcuts import render, HttpResponse, redirect
@@ -17,8 +10,9 @@ import time
 import threading
 from django.urls import reverse
 
-from .models import *
+# from .models import *
 
+import math
 # Create your views here.
 
 def index(request):
@@ -40,8 +34,10 @@ POSE_PAIRS = [ ["Head", "Neck"], ["Neck", "RShoulder"], ["RShoulder", "RElbow"],
 
 # 각 파일 path
 BASE_DIR = Path(__file__).resolve().parent
-protoFile = "bodygree\pose_deploy_linevec_faster_4_stages.prototxt"
-weightsFile = "bodygree\pose_iter_160000.caffemodel"
+# protoFile = "bodygree\pose_deploy_linevec_faster_4_stages.prototxt"
+# weightsFile = "bodygree\pose_iter_160000.caffemodel"
+protoFile = "bodygree/pose_deploy_linevec_faster_4_stages.prototxt"
+weightsFile = "bodygree/pose_iter_160000.caffemodel"
 
 # 위의 path에 있는 network 모델 불러오기
 net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
@@ -52,51 +48,31 @@ net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 
 status=2
 keep_time=[0,0,0]
-O_X_HandsUp=["O","X","HandsUp"]
+points = []
 Success_Fail=["Success","Fail"]
 
 return_result=""
 
 ############################################
 # 1초마다 자세를 체크하도록 코드를 짰다. => threading.Timer 사용
-
-def check_timer1(pose_type):  #O,X,HandsUp
-    global keep_time,status
-    global return_result
-    if keep_time[0]==5 or keep_time[1]==5 or keep_time[2]==8:
-        result=show_result(pose_type,status) #O,X,HandsUp / Again
-        if result in O_X_HandsUp:
-            return_result=result
-        elif result=="Again":
-            keep_time[0]=keep_time[1]=keep_time[2]=0 #시간 초기화
-    
-    elif keep_time[status]==0:
-        #다른 모션에서 바뀌어 들어옴
-        keep_time[0]=keep_time[1]=keep_time[2]=0
-        keep_time[status]+=1
-
-    elif keep_time[status]!=0:
-        keep_time[status]+=1
-
-    print(keep_time[0],keep_time[1],keep_time[2])
-    
-    if return_result=="":
-        threading.Timer(1.0,check_timer1,(pose_type,)).start()
         
-def check_timer2(pose_type):  #Stretching
+def check_timer2(pose_type):
     global keep_time,status
     global return_result
-    if keep_time[0]==10 or keep_time[2]==8: # 자세유지 10초 하면 끝
-        result=show_result(pose_type,status) #Success / Fail
-        return_result=result
-    
-    elif keep_time[status]==0:
-        #다른 모션에서 바뀌어 들어옴
-        keep_time[0]=keep_time[2]=0
-        keep_time[status]+=1
+    if show_result(pose_type,status) =='Success':
+        if keep_time[0]==10 or keep_time[2] == 10: # 자세유지 10초 하면 끝
+            result=show_result(pose_type,status) #Success / Fail
+            return_result=result
+        
+        elif keep_time[status]==0:
+            #다른 모션에서 바뀌어 들어옴
+            # keep_time[0]=keep_time[2]=0
+            keep_time[status]+=1
 
-    elif keep_time[status]!=0:
-        keep_time[status]+=1
+        elif keep_time[status]!=0:
+            keep_time[status]+=1
+    else:
+        keep_time[1] +=1
 
     print(keep_time[0],keep_time[1],keep_time[2])
     
@@ -107,136 +83,46 @@ def check_timer2(pose_type):  #Stretching
 ##################################################
 # 자세 체크 함수들
 
-def check_HandsUp(points):
-    if points[0] and points[2] and points[3] and points[5] and points[6]:
+def check_body(points):
+    if points[0] and points[2] and points[5] and points[8] and points [11]:
         head_x,head_y=points[0] #머리
         rs_x,rs_y=points[2] #오른쪽 어깨
-        re_x,re_y=points[3] #오른쪽 팔꿈치
         ls_x,ls_y=points[5] #왼쪽 어깨
-        le_x,le_y=points[6] #왼쪽 팔꿈치
-        
-        #팔꿈치가 어깨보다 높을 것, 양 팔꿈치 사이에 머리가 위치할 것
-        if re_y < rs_y and le_y < ls_y  and re_x < head_x and head_x < le_x: 
-            
-            if points[4] and points[7]: 
-                rw_x,rw_y=points[4] #오른쪽 손목
-                lw_x,lw_y =points[7] #왼쪽 손목
+        rh_x,rh_y=points[8] #오른쪽 골반
+        lh_x,lh_y=points[11] #왼쪽 골반
 
-                #양쪽 손목 중, 어느 하나라도 머리보다는 위에 가야한다.
-                if rw_y<head_y or lw_y < head_y:  
-                    return True
-                else:
-                    return False
-                
+        if rs_y > ls_y or rs_y < ls_y or rh_y > lh_y or rh_y < lh_y: #어깨 높이나 골반 높이가 차이 남
+            s_gradient = (rs_y-ls_y)/(rs_x-ls_x) #어깨 기울기
+            h_gradient = (rh_y-lh_y)/(rh_x-lh_x) #골반 기울기
+            degree = math.radians(5)
+            standard_gradient = math.tan(degree)
+            
+            if s_gradient > standard_gradient or h_gradient > standard_gradient:
+                return True
             else:
                 return False
-            
-        else:
-            return False
-        
-    else:
-        return False
-
-def check_O(points):
-    if points[3] and points[4] and points[6] and points[7]:
-        re_x,re_y=points[3] #오른쪽 팔꿈치
-        rw_x,rw_y=points[4] #오른쪽 손목
-        le_x,le_y=points[6] #왼쪽 팔꿈치
-        lw_x,lw_y=points[7] #왼쪽 손목
-        
-        #기본적으로 만세 조건을 만족시킬것 check_HandsUP()
-        #손목이 팔꿈치보다 안쪽에 위치할 것
-        #손목이 팔꿈치보다 위쪽에 위치할 것
-        if check_HandsUp(points) and ( (re_x<rw_x and rw_y<re_y) or (le_x>lw_x and le_y>lw_y) ):
-            return True
-        else:
-            return False
-        
-    else:
-        return False
-    
-def check_X(points):
-    if points[0] and points[2] and points[3] and points[4] and points[5] and points[6] and points[7] and points[8]:
-        head_x,head_y=points[0] #머리
-        rs_x,rs_y=points[2] #오른쪽 어깨
-        re_x,re_y=points[3] #오른쪽 팔꿈치
-        rw_x,rw_y=points[4] #오른쪽 손목
-        ls_x,ls_y=points[5] #왼쪽 어깨
-        le_x,le_y=points[6] #왼쪽 팔꿈치
-        lw_x,lw_y=points[7] #왼쪽 손목
-        b_x,b_y=points[8]  #골반(오른쪽)
-        
-        #골반보다 팔꿈치가 위쪽에 위치 and 팔꿈치보다 손목이 위쪽에 위치 and 손목보다 머리가 위쪽에 위치
-        #어깨 안쪽으로 손목이 위치
-        if (b_y>le_y and b_y>re_y) and (le_y>lw_y and re_y >rw_y) and(lw_y>head_y and rw_y>head_y):
-            if rs_x<rw_x or lw_x<ls_x:
-                r_gradient=-1
-                l_gradient=1
-                if rw_x-re_x !=0:
-                    r_gradient= (rw_y-re_y)/(rw_x-re_x)
-                if lw_x-le_x !=0:
-                    l_gradient= (lw_y-le_y)/(lw_x-le_x)
-
-                if r_gradient<0 or l_gradient>0:
-                    return True
-                else:
-                    return False        
-    else:
-        return False   
-
-def check_Stretching(points):
-    #오른쪽 골반, 오른쪽 무릎, 오른쪽 발목,  왼쪽 골반, 왼쪽 무릎, 왼쪽 발목
-    if points[8] and points[9] and points[10] and points[11] and points[12] and points[13]:
-        rh_x,rh_y=points[8] #오른쪽 골반
-        rk_x,rk_y=points[9] #오른쪽 무릎
-        ra_x,ra_y=points[10] #오른쪽 발목
-        lh_x,lh_y=points[11] #왼쪽 골반
-        lk_x,lk_y=points[12] #왼쪽 무릎
-        la_x,la_y=points[13] #왼쪽 발목
-        
-        #기본적으로 O 조건을 만족시킬 것 check_O() 
-        if check_O(points): 
-            #오른쪽을 구부림
-            if rk_x<rh_x and rk_x<ra_x and ra_y<la_y:
-                return True
-            #왼쪽을 구부림
-            elif lk_x>lh_x and lk_x>la_x and la_y<ra_y:
-                return True
-        else:
-            return False
-    
-    else:
-        return False
-
 
 #################################################
 # 결과를 반환하는 함수
 
 def show_result(pose_type,status): #END/Again
-    if status==2: #status 2
-        if pose_type=="Stretching":
-            print("자세 유지에 실패하셨습니다.") 
-            return "Fail"
-        else:
-            print("자세를 취해주세요.") 
-            return "Again"
-        
-    elif status==1: #status 1
-        print("X를 선택하셨습니다.")
-        return "X"
-        
-    else: #status 0
-        if pose_type=="OX":
-            print("O를 선택하셨습니다.")
-            return "O"
+    global points
+    
+    try:
+        if points[0] and points[2] and points[5] and points[8] and points[11]:
+            if status==0: #status 0
+                if pose_type=="bodycheck":
+                    print("자세 검출중 (척추 측만증)") 
+                    return "Success"
             
-        elif pose_type=="XHandsUp":
-            print("만세를 선택하셨습니다.")
-            return "HandsUp"
-        
-        elif pose_type=="Stretching":
-            print("자세유지에 성공하셨습니다.")
-            return "Success"
+            elif status==2: #status 2     
+                if pose_type=="bodycheck":
+                    print("자세 검출중 (정상 척추)")
+                    return "Success"
+            else:
+                return "Again"
+    except:
+        return "Again"
             
 ##################################################
 
@@ -252,8 +138,6 @@ class Openpose(object):
     def __del__(self):
         self.video.release()
         cv2.destroyAllWindows()
-        
-
     
     def get_frame(self,pose_type):
         global status,return_result
@@ -275,6 +159,7 @@ class Openpose(object):
         output = net.forward() #추론을 진행할때 이용하는 함수
         
         # 키포인트 검출시 이미지에 그려줌
+        global points
         points = []
         for i in range(0,15):
             # 해당 신체부위 신뢰도 얻음.
@@ -294,46 +179,21 @@ class Openpose(object):
                 points.append((int(x), int(y)))
             else :
                 points.append(None)
-             
-        #OX일때   
-        if pose_type=="OX":
-            if check_O(points):
+                             
+        #bodycheck일때
+        if pose_type=="bodycheck":
+            if check_body(points):
                 status=0
-                cv2.putText(frame, "choose O" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
-            elif check_X(points):
-                status=1
-                cv2.putText(frame, "choose X" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
+                cv2.putText(frame, "Bad Spine status" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
             else:
                 status=2
-                cv2.putText(frame, "None" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
-        
-        #XHandsUp일때   
-        elif pose_type=="XHandsUp":       
-            if check_HandsUp(points):
-                cv2.putText(frame, "choose HandsUp" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
-                status=0
-            elif check_X(points):
-                status=1
-                cv2.putText(frame, "choose X" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
-            else:
-                status=2
-                cv2.putText(frame, "None" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
-                
-        #Stretching일때   
-        elif pose_type=="Stretching":       
-            if check_Stretching(points):
-                status=0
-                cv2.putText(frame, "Keep going" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
-            else:
-                status=2
-                cv2.putText(frame, "None" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
+                cv2.putText(frame, "Good Spine status" , (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 1, lineType=cv2.LINE_AA)
 
         #결과가 결정이 나면 반환
-        if return_result in O_X_HandsUp or return_result in Success_Fail:
+        if return_result in Success_Fail:
             return return_result
         
         
-
         # 각 POSE_PAIRS별로 선 그어줌 (머리 - 목, 목 - 왼쪽어깨, ...)
         for pair in POSE_PAIRS:
             partA = pair[0]             # Head
@@ -348,21 +208,15 @@ class Openpose(object):
         #self.fps.update()
         _, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
-            
-    
     
 def gen(camera,pose_type):
-    #Stretching과 OX,XHandsUp의 timer가 다르다.
-    if pose_type=="Stretching":
-        check_timer2(pose_type)
-    else:
-        check_timer1(pose_type)
-        
+    check_timer2(pose_type)
+    
     while True:
         frame = camera.get_frame(pose_type)
         
         #결과가 나오면 카메라를 끄고 반복문 break
-        if frame in O_X_HandsUp or frame in Success_Fail:  
+        if frame in Success_Fail: 
             del camera
             break
         else:
@@ -375,25 +229,9 @@ def gen(camera,pose_type):
 # detectme 관련 함수들
 
 @gzip.gzip_page
-def detectme_OX(request):
+def detectme_bodycheck(request):
     try:
-        return StreamingHttpResponse(gen(Openpose(),"OX"), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
-        print("에러입니다...")
-        pass
-    
-@gzip.gzip_page
-def detectme_XHandsUp(request):
-    try:
-        return StreamingHttpResponse(gen(Openpose(),"XHandsUp"), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
-        print("에러입니다...")
-        pass
-    
-@gzip.gzip_page
-def detectme_Stretching(request):
-    try:
-        return StreamingHttpResponse(gen(Openpose(),"Stretching"), content_type="multipart/x-mixed-replace;boundary=frame")
+        return StreamingHttpResponse(gen(Openpose(),"bodycheck"), content_type="multipart/x-mixed-replace;boundary=frame")
     except:  # This is bad! replace it with proper handling
         print("에러입니다...")
         pass
@@ -403,19 +241,23 @@ def detectme_Stretching(request):
 # 링크 관련 함수들
 
 def home(request):
-    return render(request,"bodygree/home.html") #home.html을 호출해서 띄워준다.
- 
+    return render(request, "bodygree/home.html") #home.html을 호출해서 띄워준다.
+
+def login(request):
+    return render(request, "bodygree/login.html")
+
 def HTMLTemplate(request,pose_type):
     return render(request,"bodygree/HTMLTemplate.html",{"pose_type":pose_type})
 
-def OX(request):
-    pose_type="OX"
+def Bodycheck(request):
+    pose_type="bodycheck"
     return HttpResponse(HTMLTemplate(request,pose_type))
 
-def XHandsUp(request):
-    pose_type="XHandsUp"
-    return HttpResponse(HTMLTemplate(request,pose_type))
+def dignore(request):
+    return render(request, "bodygree/dignore.html")
 
-def Stretching(request):
-    pose_type="Stretching"
-    return HttpResponse(HTMLTemplate(request,pose_type))
+def result(request):
+    if keep_time[0] == 10:
+        return render(request, "bodygree/result_bad.html")
+    elif keep_time[2] == 10:
+        return render(request, "bodygree/result_good.html")
